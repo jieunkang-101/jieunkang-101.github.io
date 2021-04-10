a2616e6d5604b02b1d6d5491d6bf8068168d9a3b
Jieun Kang <jieunkang101@gmail.com>
2021-04-10 14:52:08 -0700

    fix hamburger menu display issue and add scroll to section

--- main.js
+++ main.js
@@ -10,3 +10,22 @@ document.addEventListener('scroll', () => {
     navbar.classList.remove('navbar--dark');
   }
 });
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({behavior: "smooth"});
+  navbarMenu.classList.remove('open');
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
